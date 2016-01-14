07de14cb374359c39c46da030bfabd78c4c14021
Roberto <rmartin.martin@gmail.com>
2016-01-13 23:45:15 -0500

    moving article editing to admin section

---
+++ app/controllers/admin/articles_controller.rb
@@ -0,0 +1,34 @@
+class Admin::ArticlesController < ApplicationController
+  before_filter :authenticate_user!
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    #todo: handle article update
+  end
+
+  def destroy
+    #todo: handle destroy
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :articles
   end
 
-  resources :articles do
+  resources :articles, only: [:show, :index] do
     resources :comments
   end
   # The priority is based upon order of creation: first created -> highest priority.
