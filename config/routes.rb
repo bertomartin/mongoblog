7289cd5567a8296ab71ff245620f117e4f43d808
Roberto <rmartin.martin@gmail.com>
2016-01-16 21:53:14 -0500

    adding page models/view/controllers in admin area

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '', to: 'dashboard#index', as: '/'
     resources :articles
+    resources :pages
   end
 
   resources :articles, only: [:show, :index] do
